cee72663eb1d82723dfbb0202359b272b3bfe08e
Taylor Wu <Taylorwu21@gmail.com>
2016-10-24 19:01:01 -0600

    init commit

---
+++ src/assignment1/Assignment1.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1;
+
+/**
+ *
+ * @author taylorwu
+ */
+public class Assignment1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Taylor Wu");
+        System.out.println("TaylorWu21@gmail.com");
+        System.out.println("801-123-1234");
+ 
+        System.out.println("X                      X");
+        System.out.println("X                      X");
+        System.out.println("X      XXXXXXXXXX      X");
+        System.out.println("XXXXX  X        X  XXXXX");
+        System.out.println("X   X  X        X  X   X");
+        System.out.println("X   X  X        X  X   X");
+        
+        
+        String motto = "Carly's makes the food that makes it a party";
+        
+        System.out.println("**********************************************");
+        System.out.println("*" + motto + "*");
+        System.out.println("**********************************************");
+    }
+    
+    
+}
